0a1a74c0e226b08634d826ae9ffcd8486a8b5903
RiShBhs-Code <rishbhh1617@gmail.com>
2025-10-19 14:43:13 +0530

    SampleAPITest.java modified

--- src/test/java/com/apitests/SampleAPITest.java
+++ src/test/java/com/apitests/SampleAPITest.java
@@ -1,24 +1,48 @@
-package com.apitests;
+package com.example.tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import static io.restassured.RestAssured.given;
 
 public class SampleAPITest {
-// A simple, reliable API test using REST-Assured
-@Test
-public void verifyGitHubStatusIs200() {
-given()
-.when()
-.get(&quot;https://api.github.com/users/octocat&quot;)
-.then()
-.statusCode(200); // Asserts the response code is 200 (Success)
-}
-// You can add more tests here, like a failure example
-@Test
-public void verifyInvalidEndpointStatusIs404() {
-given()
-.when()
-.get(&quot;https://api.github.com/nonexistent&quot;)
-.then()
-.statusCode(404); // Asserts the response code is 404 (Not Found)
-}
+
+    @BeforeClass
+    public void setup() {
+        // Base URL for the API
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+
+    @Test
+    public void verifyGetPostAPI() {
+        Response response = RestAssured
+                .given()
+                .when()
+                .get("/posts/1")
+                .then()
+                .extract()
+                .response();
+
+        // Print response for debugging
+        System.out.println("Response: " + response.asString());
+
+        // Basic validations
+        Assert.assertEquals(response.statusCode(), 200, "Status code mismatch!");
+        Assert.assertTrue(response.jsonPath().getString("title") != null, "Title is missing!");
+    }
+
+    @Test
+    public void verifyUserAPI() {
+        Response response = RestAssured
+                .given()
+                .when()
+                .get("/users/1")
+                .then()
+                .extract()
+                .response();
+
+        Assert.assertEquals(response.statusCode(), 200);
+        Assert.assertEquals(response.jsonPath().getString("username"), "Bret");
+    }
 }
